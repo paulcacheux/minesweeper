0a165acf5dc5ea08f2825de2ec948f1b5cc85468
paulcacheux <paulcacheux@gmail.com>
2019-11-11 18:19:06 +0000

    Deploying to gh-pages from master 82f8637dd324d41585b62853e7698bd3d26828ee

---
+++ precache-manifest.c873c54a64fead3dfd47f7b2b70d6603.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e1b6fd78905f2dcd08e9df7e322e50f",
+    "url": "/minesweeper/index.html"
+  },
+  {
+    "revision": "fd7a1bbf15af4a148772",
+    "url": "/minesweeper/static/css/main.8747c069.chunk.css"
+  },
+  {
+    "revision": "43e2c8b2e5a0bf46143f",
+    "url": "/minesweeper/static/js/2.ca5a9693.chunk.js"
+  },
+  {
+    "revision": "fd7a1bbf15af4a148772",
+    "url": "/minesweeper/static/js/main.e1c5b7d0.chunk.js"
+  },
+  {
+    "revision": "546a098e25c3db089e5d",
+    "url": "/minesweeper/static/js/runtime-main.a7fec516.js"
+  },
+  {
+    "revision": "c7a5b63ebd65324cd3367380c4663f98",
+    "url": "/minesweeper/static/media/bomb.c7a5b63e.png"
+  }
+]);
